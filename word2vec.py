5d03ff5150f87b4ffeb955827f09c8663a5f9ba4
Ho-01 <kimhobig@gmail.com>
2023-11-28 11:53:32 +0900

    'heatmap'

--- word2vec.py
+++ word2vec.py
@@ -3,22 +3,26 @@ from word_tokenize import word_tokenized_transcript
 from gensim.models import Word2Vec
 
 
-num_workers = 4 # 실행할 병렬 프로세스의 수, 코어수. 주로 4~6 사이 지정
-min_word_count = 5 # 단어에 대한 최소 빈도수. min_count=5라면 빈도수 5 이하 무시
-size = 300 # 각 단어에 대한 임베딩 된 벡터 차원 정의, size=2라면 한 문장의 벡터는 [-0.1248574, 0.255778]과 같은 형태를 가지게 된다.
-window = 10 # 문맥 윈도우 수. 양쪽으로 몇 개의 단어까지 고려해서 의미를 파악할 것인지 지정하는 것
+workers = 4 # 실행할 병렬 프로세스의 수, 코어수. 주로 4~6 사이 지정
+min_count = 1 # 단어에 대한 최소 빈도수. min_count=5라면 빈도수 5 이하 무시
+vector_size = 300 # 각 단어에 대한 임베딩 된 벡터 차원 정의, size=2라면 한 문장의 벡터는 [-0.1248574, 0.255778]과 같은 형태를 가지게 된다.
+window = 20 # 문맥 윈도우 수. 양쪽으로 몇 개의 단어까지 고려해서 의미를 파악할 것인지 지정하는 것
 sample = 1e-3 # 빠른 학습을 위해 정답 단어 라벨에 대한 다운샘플링 비율을 지정하는 것, 보통 0.001이 좋은 성능을 냄
+sg = 0 # 0은 CBOW방식, 1은 skip-grows 방식
 
 # 단어_토큰화된 문장들을 이용해서 벡터를 생성한다
 # Word2Vec( sentences, workers, size, min_count, window, sample)
 model = Word2Vec(sentences=word_tokenized_transcript,
-                 workers=num_workers,
-                 vector_size=size,
-                 min_count=min_word_count,
+                 workers=workers,
+                 min_count=min_count,
+                 vector_size=vector_size,
                  window=window,
-                 sample=sample)
+                 sample=sample,
+                 sg=sg)
 
-
-model_result = model.wv.most_similar("Brazil")
+print(model.cum_table)
+model_result = model.wv.most_similar("final")
 print(model_result)
+# print(model.wv['score'])
+
 
--- word_tokenize.py
+++ word_tokenize.py
@@ -6,20 +6,31 @@ nltk.download()
 ''' # NLTK 다운로드는 첫 1회만 수행하면 된다
 
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 
+import re
 
 # URL : https://www.youtube.com/watch?v=X7158uQk1yI
-# Title : FULL MATCH: Brazil vs. France 2006 FIFA World Cup
+# Title : FULL MATCH: Brazil vs. France 200  v6 FIFA World Cup
 transcript_array = YouTubeTranscriptApi.get_transcript("X7158uQk1yI")
 print(transcript_array)
 
+temp_sentance = ""
+for i in range(0, len(transcript_array)):
+    temp_sentance += transcript_array[i]['text']
+print(temp_sentance)
+
+temp_sentance = sent_tokenize(temp_sentance)
+print(temp_sentance)
 
 # transcript 에 ["문장1", "문장2", "문장3", .... ] 의 형태로 저장
 transcript = []
 for i in range(0, len(transcript_array)):
-    print(transcript_array[i]['text'])
-    transcript.append(transcript_array[i]['text'])
+    st = transcript_array[i]['text'] # st에 문장 저장
+    st = st.lower() # st를 소문자로 변환
+    st = re.sub('[^a-z0-9-_. ]','',st) # st의 글자가 a~z, 0~9, - _ . (띄어쓰기) 가 아니라면 지운다
+    transcript.append(st)
 
 
 # word_tokenized_transcript 에 [ ["단어1", "단어2", ...], ["단어3", "단어4", ...], [...], .... ] 의 형태로 토큰화해 저장
@@ -41,7 +52,7 @@ for i in range(0, len(transcript)):
     # 토큰화+불용어 제거 완료된 문장을 word_tokenized_transcript 에 append
     word_tokenized_transcript.append(result)
 
-print(word_tokenized_transcript)
+# print(word_tokenized_transcript)
 
 
 
