fb4b480ce12076f1e195cd5492ba8f4edff262d7
DaanChoi <ekdksdl11@gmail.com>
2023-12-22 01:21:55 +0900

    delete

--- url_to_heatmap_test.py
+++
@@ -1,6 +0,0 @@
-import youtube_url_to_heatmap_coordinates as heatmap
-
-url = ["https://www.youtube.com/watch?v=gtvWiwvJT3M","https://www.youtube.com/watch?v=2Wuoqt29e2A","https://www.youtube.com/watch?v=3-BZxvKT6x4"]
-
-for url in url:
-    print(heatmap.youtube_url_to_heatmap_coordinates(url))
